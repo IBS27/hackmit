7c465d63f5e55a6e59ab6afeabf6c77a41b2adf3
Ayat Noor <ayat13noor@gmail.com>
2025-09-14 10:45:54 -0400

    UI changes

--- smart-glasses/src/frontend/main.tsx
+++ smart-glasses/src/frontend/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import "@fontsource/libre-franklin/400.css"; 
+import "@fontsource/arimo/400.css";  
+import "@fontsource/arimo/700.css";  
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
